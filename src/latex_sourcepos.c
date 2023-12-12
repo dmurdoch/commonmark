f6f1dddc85131e6f0a69f90df765e235c8795177
Duncan Murdoch <murdoch.duncan@gmail.com>
2023-12-12 11:38:15 -0500

    Insert \sourcepos{startline:startcol} macros, once per line.

--- src/latex_sourcepos.c
+++ src/latex_sourcepos.c
@@ -2,21 +2,39 @@
 #include <Rinternals.h>
 #include <stdlib.h>
 #include "latex_sourcepos.h"
+#include "node.h"
 
 cmark_node* latex_sourcepos(cmark_node *root) {
 
   cmark_event_type ev_type;
   cmark_iter *iter = cmark_iter_new(root);
 
+  // Only insert one macro per line
+  int lastline = -1;
+
   while ((ev_type = cmark_iter_next(iter)) != CMARK_EVENT_DONE) {
 
     cmark_node *cur = cmark_iter_get_node(iter);
 
-    if (cmark_node_get_type(cur) == CMARK_NODE_TEXT) {
+    cmark_node_type type = cmark_node_get_type(cur);
 
-      const char * literal = cmark_node_get_literal(cur);
+    // Insert sourcepos before some nodes
 
-      Rprintf("Text node: %s\n", literal);
+    if ((type == CMARK_NODE_TEXT ||
+         type == CMARK_NODE_CODE ||
+         type == CMARK_NODE_CODE_BLOCK ||
+         type == CMARK_NODE_HTML_INLINE) &&
+        lastline != cur->start_line) {
+
+      lastline = cur->start_line;
+      cmark_node *sourcepos = cmark_node_new(CMARK_NODE_CUSTOM_INLINE);
+
+      char buffer[100];
+      snprintf(buffer, 100, "\\sourcepos{%d:%d}",
+                 cur->start_line, cur->start_column);
+
+      cmark_node_set_on_enter(sourcepos, buffer);
+      cmark_node_insert_before(cur, sourcepos);
     }
   }
 
